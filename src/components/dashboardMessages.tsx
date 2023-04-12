51be6b25f16434d34b441dbd4f816f0e01017e7f
Soumajit Ghosh <soumojitghosh02@gmail.com>
2023-04-12 21:27:37 +0530

    database migrated

--- src/components/dashboardMessages.tsx
+++ src/components/dashboardMessages.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const DashboardMessages: React.FunctionComponent<{ message: string }> = ({ message }) => (
   <div className={"text-center border-2 min-w-full rounded-lg border-dashed border-primary bg-base-100/50 p-36 prose"}>
-    <h2 className={"text-3xl"}>{message}</h2>
+    <h2 className={"text-xl lg:text-3xl"}>{message}</h2>
   </div>
 );
 
